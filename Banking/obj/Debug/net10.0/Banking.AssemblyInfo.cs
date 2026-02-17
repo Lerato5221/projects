ab9f85cf650c2457736e09ad72f58be59f6a84e8
Lerato Motholo <lerato6221@gmail.com>
2026-02-17 09:24:01 +0200

    Office Pull

--- Banking/obj/Debug/net10.0/Banking.AssemblyInfo.cs
+++ Banking/obj/Debug/net10.0/Banking.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Banking")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6eda457c92a8e0c2515482b9475c17f6283801b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3063e3f64c5d344b390602a3dfe8d0fdd94b4ead")]
 [assembly: System.Reflection.AssemblyProductAttribute("Banking")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Banking")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
